3b4ab6613e68922a2ea2ad239c46bf0034f63ac1
JKhan01 <jkhan266@gmail.com>
2022-05-19 20:35:31 +0530

    Python Flask implementation completed and tested.

---
+++ shopify_multipass_flask/sample_credentials.py
@@ -0,0 +1,25 @@
+userData = {
+  "email": "jkhan266work@gmail.com",
+#   "created_at": "2013-04-11T15:16:23-04:00",
+  "first_name": "Bob",
+  "last_name": "Bobsen",
+#   "tag_string": "canadian, premium",
+#   "identifier": "bob123",
+#   "remote_ip": "107.20.160.121",
+#   "return_to": "http://yourstore.com/some_specific_site",
+  "addresses": [
+    {
+      "address1": "123 Oak St",
+      "city": "Ottawa",
+      "country": "Canada",
+      "first_name": "Bob",
+      "last_name": "Bobsen",
+      "phone": "555-1212",
+      "province": "Ontario",
+      "zip": "123 ABC",
+      "province_code": "ON",
+      "country_code": "CA",
+      "default": True
+    }
+  ]
+}
